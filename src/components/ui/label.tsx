004f56ded2e79ecfe72a1a74bd70c1e503c684fd
Bhdrpkcn <bhdrpkcn@gmail.com>
2025-02-19 13:19:49 +0300

    deploy 4

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,16 +1,38 @@
-// Label component extends from shadcnui - https://ui.shadcn.com/docs/components/label
+// // Label component extends from shadcnui - https://ui.shadcn.com/docs/components/label
+
+// "use client";
+// import * as React from "react";
+// import * as LabelPrimitive from "@radix-ui/react-label";
+
+// import { cn } from "@/lib/utils";
+
+// const Label = React.forwardRef<
+//   React.ElementRef<typeof LabelPrimitive.Root>,
+//   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
+// >(({ className, ...props }, ref) => (
+//   <LabelPrimitive.Root
+//     ref={ref}
+//     className={cn(
+//       "text-sm font-medium text-black dark:text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+//       className
+//     )}
+//     {...props}
+//   />
+// ));
+// Label.displayName = LabelPrimitive.Root.displayName;
+
+// export { Label };
 
 "use client";
-import * as React from "react";
-import * as LabelPrimitive from "@radix-ui/react-label";
 
+import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
+  HTMLLabelElement,
+  React.LabelHTMLAttributes<HTMLLabelElement>
 >(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
+  <label
     ref={ref}
     className={cn(
       "text-sm font-medium text-black dark:text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
@@ -19,6 +41,7 @@ const Label = React.forwardRef<
     {...props}
   />
 ));
-Label.displayName = LabelPrimitive.Root.displayName;
+
+Label.displayName = "Label";
 
 export { Label };
